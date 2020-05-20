a0b6efbdab8fed8fa9edf3a40a384261d7530c2d
lotostoii <lotostoii@gmail.com>
2020-05-20 16:14:50 +1000

    sdsd

--- models/server/S_cotalog.php
+++ models/server/S_cotalog.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace models\server;
 
 class S_cotalog
@@ -18,10 +19,25 @@ class S_cotalog
         $val1 = $_POST['startN'];
         $val2 = $_POST['finishN'];
 
-        $arr = $this->db->select("SELECT * FROM " . COTALOG . " WHERE  id >= :val1 AND id<=:val2", [':val1' => $val1, ':val2' => $val2]);
+        if ($_SESSION['dataForSearch'] == '') {
+            $arr = $this->db->select("SELECT * FROM " . COTALOG . " WHERE  id >= :val1 AND id<=:val2", [':val1' => $val1, ':val2' => $val2]);
+        } else {
+            $search = $_SESSION['dataForSearch'];
 
-        echo json_encode(['cotalog' => $arr, 'id_user' => $_SESSION['id_user_entry']]);
+            $fullArr = $this->db->select("SELECT * FROM " . COTALOG . " WHERE name LIKE :name COLLATE utf8_general_ci", [':name' => "%" . $search . "%"]);
+            $j = 0;
+            for ($i = $val1; $i <= $val2; $i++) {
+                if ($fullArr[$i]) {
+                    $arr[$j++] = $fullArr[$i];
+                }
+            }
+            if (!$fullArr[$val2+1]) {
+                $button_off = true; 
+            }
+        }
 
+
+
+        echo json_encode(['cotalog' => $arr, 'id_user' => $_SESSION['id_user_entry'], 'button_off' => $button_off]);
     }
-
 }
